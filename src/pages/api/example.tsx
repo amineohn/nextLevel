c49a44b59b434106710df61d518fb013f5ea1742
Amine <amineprojet7@gmail.com>
2022-08-17 15:16:53 +0200

    chore: update packages, feat(api): added oauth discord

--- src/pages/api/example.tsx
+++ src/pages/api/example.tsx
@@ -4,7 +4,6 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === "POST") {
         return res.status(200).send("OK");
     }
-
     return res.status(405).send("Method Not Allowed");
 };
 
